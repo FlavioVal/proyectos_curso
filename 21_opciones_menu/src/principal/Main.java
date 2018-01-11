ca8a637875ddcf4255ca039a632d8fa6ba799bb6
flaviov <flavio3738@gmail.com>
2018-01-11 23:32:36 +0100

    primer cambio

--- 21_opciones_menu/src/principal/Main.java
+++ 21_opciones_menu/src/principal/Main.java
@@ -15,24 +15,56 @@ import java.util.Scanner;
 public class Main {
 	static ArrayList<Float> listanotas=new ArrayList<>();
 	static ArrayList<Float> listAprobados=new ArrayList<>();
-	Float nota;
-	static Float media;
 	
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
+		int op;
 		
-		menu();
+		do {
+			System.out.println("1.- Añadir nota");
+			System.out.println("2.- Ver aprobados");
+			System.out.println("3.- Nota media");
+			System.out.println("4.- Mostrar todas");
+			System.out.println("5.- Salir");
+			op=Integer.parseInt((sc.nextLine()));	
+			switch(op) {
+			case 1:
+				System.out.print("Nota:");
+				Float nota=Float.parseFloat(sc.nextLine());
+				addNota(nota);
+				break;
+			case 2:
+				ArrayList<Float> ap=aprobados();
+				for(Float n:ap) {
+					System.out.print(n+" ; ");
+				}
+				break;
+			case 3:
+				System.out.println("La media es: "+media());
+				break;
+			case 4:
+				mostrarNotas();
+				break;
+			case 5:
+				break;			
+			default:
+				System.out.println("Ingrese nota valida.");
+			}
+		}while(op!=5);
+		
 
-	}
+	}//fin de main.
 	
-	private static void addNota(Float nota) {
-		listanotas.add(nota);
+	private static void addNota(Float n) {
+		if(n<=0 && n<=10) {
+			listanotas.add(n);
+		}
 	}
 	
 	private static ArrayList<Float> aprobados() {
 		
 		for(Float n:listanotas) {
-			if(n<5) {
+			if(n>5) {
 				listAprobados.add(n);
 			}
 		}
@@ -41,28 +73,17 @@ public class Main {
 	}
 	
 	private static Float media() {
-		
+		Float media=(float)0;
 		for(Float n:listanotas) {
 			media=media+n;
 		}
-		media=media/listanotas.size();
-		return media;
+		return (media=media/listanotas.size());	 
 	}
 	
 	private static void mostrarNotas() {
 		for(Float n:listanotas) {
-			System.out.println(n);
+			System.out.println(n+ " ; ");
 		}
 	}
 	
-	private static void menu() {
-		System.out.println("************* Menu *************");
-		System.out.println("1.- Añadir nota");
-		System.out.println("2.- Ver aprobados");
-		System.out.println("3.- Nota media");
-		System.out.println("4.- Añadir nota");
-		System.out.println("5.- Mostrar todas");
-		System.out.println("6.- Salir");
-	}
-
 }
