5108906d1a23458d0f07432b55a25cee1eacf287
manana <manana@Core11-21>
2018-01-17 12:15:55 +0100

    Primer commit

---
+++ 35_cajero_virtual/src/presentacion/Cajero.java
@@ -0,0 +1,23 @@
+package presentacion;
+/**
+ * Al iniciar el programa se solicita al usuario el saldo inicial y el limite de la cuenta(maxima cantidad que se puede extraer en el proceso)
+ * luego aparece el siguiente menú:
+ *1.- Ingresar
+ *2.- Extraer
+ *3.- Movimientos y saldo
+ *4.- Salir
+ *
+ *1. Se solicita la cantidad a ingresar y se procede al ingreso.
+ *2. Se solicita la cantidad a extraer, si es inferior o igual al limite se realiza la extracción.
+ *3. Muestra los movimientos realizados en la cuenta y el saldo final. cada movimiento se caracteriza por un
+ *   tipo (ingreso o extracción) y una cantidad.
+ *   Nota: Usar Herencia.
+ */
+public class Cajero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
