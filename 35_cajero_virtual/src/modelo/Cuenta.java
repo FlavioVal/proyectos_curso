8efc43ca8080c612b1d78ac5c4ba6aaa49a0ecb8
flaviov <flavio3738@gmail.com>
2018-01-22 00:45:53 +0100

    primer commit

--- 35_cajero_virtual/src/modelo/Cuenta.java
+++ 35_cajero_virtual/src/modelo/Cuenta.java
@@ -1,14 +1,23 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Cuenta {
 	private double saldo;
+	ArrayList<Double> listaIngresos;
 	
 	public Cuenta(double saldo) {
 		this.saldo=saldo;
 	}
 	
 	public void ingresar(double cant) {
-		saldo+=cant;
+		listaIngresos=new ArrayList<Double>();
+		if(cant>0) {
+			listaIngresos.add(cant);
+			saldo+=cant;			
+		}else {
+			System.out.println("Ingresar Cantidad Valida. !");
+		}
 	}
 	
 	public void extraer(double cant) {
