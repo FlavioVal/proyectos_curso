41991078f007888eec8808c6f4a528a83cb8f0df
manana <manana@Core11-21>
2018-01-30 10:28:51 +0100

    segundo commit

---
+++ 08_sumas_callable/src/principal/Inicio.java
@@ -0,0 +1,36 @@
+package principal;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import tareas.TareaSumador;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		//Creación ExecuteService
+		ExecutorService srv=Executors.newFixedThreadPool(3);
+		//Creamos objeto Callable
+		Callable<Long> cb= new TareaSumador(50);
+		//Ponemos en ejecución la tarea.
+		Future<Long> ft=srv.submit(cb);
+		while(!ft.isDone()) {
+			System.out.println("Esperando a que termine...");
+		}
+		try {
+			System.out.println("Resultado final: "+ft.get());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//termina el ExecutorService
+		srv.shutdown();
+	}
+
+}
